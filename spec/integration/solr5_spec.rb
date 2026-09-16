09db880edf3fdc9984cc09feaca9f0ad65d2942c
Max Kadel <max@notch8.com>
2026-09-16 13:01:49 +0200

    Run integration specs against docker-compose Solr instead of solr_wrapper
    solr_wrapper has no auth support, which we need for upcoming basic_auth
    coverage. docker-compose.yaml brings up a solr:9 container with the
    basic_configs core precreated via solr-precreate; CI now starts it
    before running the suite.

--- spec/integration/solr5_spec.rb
+++ spec/integration/solr5_spec.rb
@@ -1,22 +1,9 @@
 require 'spec_helper'
-require 'solr_wrapper'
 
 RSpec.describe "Solr basic_configs" do
-  SolrWrapper.default_instance_options = {
-    port: SolrWrapper.default_solr_port,
-    version: '9.5.0'
-  } 
-  SOLR_INSTANCE =  SolrWrapper.default_instance({})
-  before(:all) { SOLR_INSTANCE.start }
-  after(:all) { SOLR_INSTANCE.stop }
-
   context "basic configs" do
-    subject { RSolr.connect url: "http://localhost:#{SOLR_INSTANCE.port}/solr/basic_configs/"}
-    around(:each) do |example|
-      SOLR_INSTANCE.with_collection(name: "basic_configs", dir: File.join(FIXTURES_DIR, "basic_configs")) do |coll|
-        example.run
-      end
-    end
+    subject { RSolr.connect url: "http://localhost:#{ENV.fetch('SOLR_TEST_PORT', 8983)}/solr/basic_configs/" }
+
     describe "HEAD admin/ping" do
       it "should not raise an exception" do
         expect { subject.head('admin/ping') }.not_to raise_error
